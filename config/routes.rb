9d0f016cbb5e82502244cfc2009d4f7103624a07
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-01-13 17:57:26 -0300

    feat: Add Controller e views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:new, :create, :index]
+  root to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
